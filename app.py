226aba3fb6e59bd9f57d0e2af08e6ea86f852b3d
deforddaina <deforddaina@gmail.com>
2026-04-27 05:21:40 -0700

    Fix port configuration

--- app.py
+++ app.py
@@ -266,5 +266,7 @@ def generer_planning(heure_reveil, heure_coucher, cours):
 
     return planning 
 
+import os
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
